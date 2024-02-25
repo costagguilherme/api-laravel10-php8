8366fc0aac7c6af24e66e91acea7db80ea4b5979
Guilherme Costa <costa.gguilherme02@gmail.com>
2024-02-25 12:00:28 -0300

    2: store de usuarios

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function sendResponse(array $content, $message = 'success', $statusCode = 200)
+    {
+        return response([
+            'message' => $message,
+            'data' => $content,
+            'statusCode' => $statusCode
+        ]);
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -9,6 +10,14 @@ class UserController extends Controller
 {
     public function index(): Response
     {
-        return response(['message' => 'hello, worddddd'], 200);
+        $users = User::all();
+        return $this->sendResponse($users->toArray());
+    }
+
+    public function store(Request $request): Response
+    {
+        $user = $request->all();
+        $user = User::create($user);
+        return $this->sendResponse($user->toArray());
     }
 }
